1637a2603681cafa34003bcfd613f7f95e6e68b7
吴先森 <941478952@qq.com>
2020-11-16 00:09:50 -0800

    提交

---
+++ build/project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PRODUCT_TYPE="cx/x86"
+export ROOT=~/test/ProjectForMakefile
+export LIBS_ENV="libadd.a libafteradd.a"
+echo "root dir: "$ROOT
+echo "product type: "$PRODUCT_TYPE
+echo "libs: "$LIBS_ENV
+
