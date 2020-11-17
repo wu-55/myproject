cbd15197b6a31bb1905b87a2f7fa325b1025cf58
吴先森 <941478952@qq.com>
2020-11-16 21:42:56 -0800

    提交

--- build/project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PRODUCT_TYPE="wu/x86"
-export ROOT=/home/wu/myproject
-export LIBS_ENV="course.a student.a"
-echo "root dir: "$ROOT
-echo "product type: "$PRODUCT_TYPE
-echo "libs: "$LIBS_ENV
-
